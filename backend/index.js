5f285d92d194f9f910df1558a4ed9420ce08752d
Rithik Rithik <your-email@example.com>
2025-06-26 11:53:18 +0530

    second update

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://fullstack-gray-zeta.vercel.app/it a',
   credentials: true
 }));
 app.use(express.json());
